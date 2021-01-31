9d88e9f176e1751e0e9c2bb661cf35586f7f1966
jonobarel <jonobarel@users.noreply.github.com>
2021-01-31 18:05:08 +0200

    Bullet hits
    Bullets register a hit and are destroyed on collision with any rigidbody object.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -17,7 +17,11 @@ public class Bullet : ColorFightersBase
         
     }
 
-    void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<Player>() == owner) {
+            return;
+        }
+
         if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<Player>() != owner ) { //register a hit
             Debug.Log("Hit: " + owner.name + " --> " + other.gameObject.name);
         }
